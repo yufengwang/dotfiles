bac030507fd16d0087fc8b32bb051f05abaec4a0
Yufeng Wang <yufeng05w@gmail.com>
2014-05-17 21:03:34 -0400

    update linking script

--- link.sh
+++ link.sh
@@ -3,46 +3,48 @@
 
 HOME="`echo ~`"
 echo "Home: $HOME"
+echo ""
 
-# start in dotfiles root
-cd $HOME/.dotfiles/
+# start in dotfiles source directory
+cd $HOME/.dotfiles/src/
 
-for item in ./*; do
+for match in ./*; do
   # grab only the filename
-  file="`echo ${item:2}`"
+  file="`echo ${match:2}`"
 
-  if [ "$file" == "link.sh" ] || [ "$file" == "README.md" ]; then
-    # skip script and readme
-    echo "$file: skipping"
-    continue
-
-  elif [ -e "$HOME/.$file" ] && [ ! -L "$HOME/.$file" ] ; then
+  if [ -e "$HOME/.$file" ] && [ ! -L "$HOME/.$file" ] ; then
     # if file exists and is not a symlink
     echo ".$file: already exists"
-    echo ".$file: backing up"
+    echo ".$file: backing up to .$file.old"
     mv $HOME/.$file $HOME/.$file.old
 
     echo ".$file: symlinking"
-    ln -s $HOME/.dotfiles/$file $HOME/.$file
+    ln -s $HOME/.dotfiles/src/$file $HOME/.$file
 
   elif [ -e "$HOME/.$file" ] && [ -L "$HOME/.$file" ] ; then
     # if file exits and is a symlink
     echo ".$file: symlink already exists"
     echo ".$file: updating"
     rm -rf $HOME/.$file
-    ln -s $HOME/.dotfiles/$file $HOME/.$file
+    ln -s $HOME/.dotfiles/src/$file $HOME/.$file
 
   else
     # file does not exist
     echo ".$file: does not exist"
     echo ".$file: symlinking"
-    ln -s $HOME/.dotfiles/$file $HOME/.$file
+    ln -s $HOME/.dotfiles/src/$file $HOME/.$file
   fi
+  echo ""
 done
 
-# setup submodules for vim bundles
+# cd to dotfiles root
+cd ..
+
+# setup submodules in vim bundles
 echo "setting up submodules"
 git submodule init
 git submodule update
 
 echo "Done"
+echo ""
+
