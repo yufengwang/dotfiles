7dd51cdf327ee9ec928285b143933358510eb4ed
Yufeng Wang <yufengwang05@gmail.com>
2018-12-15 22:02:40 -0500

    refresh shell scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Clone the dotfiles repository and run the installer.
 
+set -eu -o pipefail
+
 HOME="$(echo ~)"
-DOTFILES_DIR="${HOME}/.dotfiles"
-REMOTE_URL="https://github.com/yufengwng/dotfiles.git"
+ROOT_DIR="$HOME/.dotfiles"
+REMOTE_URL='https://github.com/yufengwng/dotfiles.git'
 
-if [[ ! -d "${DOTFILES_DIR}" ]]; then
-  git clone ${REMOTE_URL} ${DOTFILES_DIR}
-  echo ""
+if [[ -d "$ROOT_DIR" ]]; then
+  printf 'dotfiles already present on system\n'
+else
+  printf '> fetching dotfiles\n'
+  git clone $REMOTE_URL $ROOT_DIR
+  printf '> running dotfiles installer\n'
+  $ROOT_DIR/bin/dotfilez install
 fi
-
-# invoke the installer
-${DOTFILES_DIR}/bin/dotfilez install
